2630b21606badb280c55232245de943609fbba45
Kiniamaro <vdyotte@gmail.com>
2016-04-11 06:48:38 -0400

    change script to use curl

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
 # Run this script as root
-# requires wget cuz I'm too lazy to use curl
+# requires python3-pip but you should have that already
 
 pip3 install pycrypto
-wget https://github.com/profi200/Project_CTR/releases/download/0.15/makerom_015_ctrtool.zip
 
-# these two files are the certificates that I uploaded to my file sharing website
+# these 4 files are the certificates and makerom and ctrtool
 # check them yourself if you don't trust me :^)
-wget https://a.maro.xyz/BkJbZtH.crt
-wget https://a.maro.xyz/YqO8dBx.key
-
-mv BkJbZtH.crt ctr-common-1.crt
-mv YqO8dBx.key ctr-common-1.key
-unzip makerom_015_ctrtool.zip
-rm -r makerom_015_ctrtool.zip MacOSX/ Windows_x86_64/
-mv Linux_x86_64/* ./
-rm -r Linux_x86_64/
+curl -o ctr-common-1.crt https://a.maro.xyz/BkJbZtH.crt
+curl -o ctr-common-1.key https://a.maro.xyz/YqO8dBx.key
+curl -o makerom https://a.maro.xyz/jKh9QF9
+curl -o ctrtool https://a.maro.xyz/AeNG5J2
 
 echo "Installation Successful!"
